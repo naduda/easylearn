afbea51b85e1dae206f8917b4bdc68c7d04a054a
slavapoet <slava@adscale.com>
2020-11-25 11:13:15 +0200

    add theme module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ThemeService } from './theme/theme.service';
+import { ETheme } from './theme/utils/ETheme';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,13 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(themeService: ThemeService) {
+    themeService.setTheme(ETheme.Main);
+  }
+}
